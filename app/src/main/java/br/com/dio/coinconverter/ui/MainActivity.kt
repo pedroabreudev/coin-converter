5416649a25673640ce7b25f1483432e4733b7247
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-09-09 15:33:57 -0400

    View Complete

--- app/src/main/java/br/com/dio/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/coinconverter/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package br.com.dio.coinconverter.ui
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import br.com.dio.coinconverter.data.model.Coin
 import br.com.dio.coinconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        bindAdapters()
+        bindListeners()
+    }
+
+    private fun bindListeners() {
+        binding.tilValue.editText?.doAfterTextChanged {
+            binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
+        }
+        binding.btnConverter.setOnClickListener{
+
+        }
+    }
+
+    private fun bindAdapters() {
+        val list = Coin.values()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        binding.tvFrom.setAdapter(adapter)
+        binding.tvTo.setAdapter(adapter)
+
+        binding.tvFrom.setText(Coin.BRL.name, false)
+        binding.tvTo.setText(Coin.USD.name, false)
     }
 }
